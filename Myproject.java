3ccac409fdeba7214421ca0dbb8e463c9d0e40e7
Kittichai <attack28052547@gmail.com>
2023-03-22 11:44:58 +0700

    main 4 command

--- Myproject.java
+++ Myproject.java
@@ -3,5 +3,6 @@ public class Myproject {
         System.out.println("Hello World");
         System.out.println("Hello Java");
         System.out.println("Hello Programing");
+        System.out.println("Hello Python");
     }
 }
